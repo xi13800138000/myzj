426c36ed3d6c2e604df4928308de4ecf09a83265
xi13800138000 <xi13800138000@gmail.com>
2025-11-23 20:59:49 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-PORT = 443
+PORT = 25355
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "tg52jo9u1bcmfn8nrpzh9kxqseu5tabc",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -21,7 +21,7 @@ MODES = {
 
 # The domain for TLS mode, bad clients are proxied there
 # Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
+TLS_DOMAIN = "www.cloudflare.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
